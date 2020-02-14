cc1834e78fa6c20a4c07257836784dceab80cd67
Alumno <Alumno@pc-PC>
2020-02-14 12:12:54 -0300

    new main class

---
+++ Oscurilandia/src/TrabajoOscurilandia/Juego.java
@@ -0,0 +1,14 @@
+package TrabajoOscurilandia;
+
+public class Juego {
+
+	public Juego() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
