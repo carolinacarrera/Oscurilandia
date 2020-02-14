c5f01c5ed4a5ccdaa2d4e340590231011e417db0
Alumno <Alumno@PC-PC>
2020-02-14 12:14:02 -0300

    tablero

--- Oscurilandia/src/TrabajoOscurilandia/Carro.java
+++ Oscurilandia/src/TrabajoOscurilandia/Carro.java
@@ -5,14 +5,12 @@ public class Carro {
 	private int ocupantes;
 	private String fechaIngreso;
 	private int posicion;
-	private int fila;
-	private int columna;
+
 	public Carro(int ocupantes, String fechaIngreso, int posicion, int fila, int columna) {
 		this.ocupantes = ocupantes;
 		this.fechaIngreso = fechaIngreso;
 		this.posicion = posicion;
-		this.fila = fila;
-		this.columna = columna;
+	
 	}
 	public int getOcupantes() {
 		return ocupantes;
@@ -32,22 +30,10 @@ public class Carro {
 	public void setPosicion(int posicion) {
 		this.posicion = posicion;
 	}
-	public int getFila() {
-		return fila;
-	}
-	public void setFila(int fila) {
-		this.fila = fila;
-	}
-	public int getColumna() {
-		return columna;
-	}
-	public void setColumna(int columna) {
-		this.columna = columna;
-	}
+	
 	@Override
 	public String toString() {
-		return "Carro [ocupantes=" + ocupantes + ", fechaIngreso=" + fechaIngreso + ", posicion=" + posicion + ", fila="
-				+ fila + ", columna=" + columna + "]";
+		return "Carro [ocupantes=" + ocupantes + ", fechaIngreso=" + fechaIngreso + ", posicion=" + posicion + "]";
 	}
 	
 	public void imprimir() {
