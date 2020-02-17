3c87a80e2e9bc3f2f70c3221a3a67b2f42a14245
Alumno <Alumno@pc-PC>
2020-02-17 12:57:06 -0300

    arraylist

--- Oscurilandia/src/TrabajoOscurilandia/Huevo.java
+++ Oscurilandia/src/TrabajoOscurilandia/Huevo.java
@@ -1,13 +1,13 @@
 package TrabajoOscurilandia;
 
-
+import java.util.ArrayList;
 
 public class Huevo {
 	
 	private int posicionFila;
 	private int posicionColumna;
 	private int puntaje;
-	
+	public ArrayList<Huevo> Huevos = new ArrayList <Huevo>();
 	
 	
 	static void Lanzamiento(){
