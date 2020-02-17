bb0cfd982fcf8eb43a41f573c0f27723f3c30d98
Alumno <Alumno@pc-PC>
2020-02-17 13:01:29 -0300

    getset

--- Oscurilandia/src/TrabajoOscurilandia/Huevo.java
+++ Oscurilandia/src/TrabajoOscurilandia/Huevo.java
@@ -10,6 +10,69 @@ public class Huevo {
 	public ArrayList<Huevo> Huevos = new ArrayList <Huevo>();
 	
 	
+	
+	@Override
+	public String toString() {
+		return "Huevo [posicionFila=" + posicionFila + ", posicionColumna=" + posicionColumna + ", puntaje=" + puntaje
+				+ ", Huevos=" + Huevos + "]";
+	}
+
+
+	public int getPosicionFila() {
+		return posicionFila;
+	}
+
+
+	public void setPosicionFila(int posicionFila) {
+		this.posicionFila = posicionFila;
+	}
+
+
+	public int getPosicionColumna() {
+		return posicionColumna;
+	}
+
+
+	public void setPosicionColumna(int posicionColumna) {
+		this.posicionColumna = posicionColumna;
+	}
+
+
+	public int getPuntaje() {
+		return puntaje;
+	}
+
+
+	public void setPuntaje(int puntaje) {
+		this.puntaje = puntaje;
+	}
+
+
+	public ArrayList<Huevo> getHuevos() {
+		return Huevos;
+	}
+
+
+	public void setHuevos(ArrayList<Huevo> huevos) {
+		Huevos = huevos;
+	}
+
+
+	public Huevo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Huevo(int posicionFila, int posicionColumna, int puntaje, ArrayList<Huevo> huevos) {
+		super();
+		this.posicionFila = posicionFila;
+		this.posicionColumna = posicionColumna;
+		this.puntaje = puntaje;
+		Huevos = huevos;
+	}
+
+
 	static void Lanzamiento(){
 		
 	}
