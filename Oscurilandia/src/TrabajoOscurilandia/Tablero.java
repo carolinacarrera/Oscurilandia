10158dc2fc43552f4832a489545578ee17c7d6af
Alumno <Alumno@pc-PC>
2020-02-17 10:43:06 -0300

    mostrar matriz

--- Oscurilandia/src/TrabajoOscurilandia/Tablero.java
+++ Oscurilandia/src/TrabajoOscurilandia/Tablero.java
@@ -9,20 +9,20 @@ import javax.swing.JOptionPane;
 
 
 public class Tablero {
-	
-	
-	public int numerosFila = 15;// numeros filas
-	public int numerosColumnas=15;// numeros columnas
+
+
+	public static int numerosFila = 15;// numeros filas
+	public static int numerosColumnas=15;// numeros columnas
 	public int carrosJugador; //carros jugadores
-	
-	public char [][] tablero = new char[numerosFila][numerosColumnas];//tamaño tablero
-	
+
+	private static char [][] tablero = new char[numerosFila][numerosColumnas];//tamaño tablero
+
 	public static List<Carro> Carros = new ArrayList <Carro>(); //debe tener un maximo de 18 objetos, carros.
 	public List<Huevo> Huevos = new ArrayList <Huevo>(); //ilimitados
-	
+
 	Scanner lector = new Scanner(System.in);
-	
-	
+
+
 	public int getNumerosFila() {
 		return numerosFila;
 	}
@@ -53,12 +53,12 @@ public class Tablero {
 	}
 
 
-	public String[][] getTablero() {
+	public char[][] getTablero() {
 		return tablero;
 	}
 
 
-	public void setTablero(String[][] tablero) {
+	public void setTablero(char[][] tablero) {
 		this.tablero = tablero;
 	}
 
@@ -100,7 +100,7 @@ public class Tablero {
 	}
 
 
-	public Tablero(int numerosFila, int numerosColumnas, int carrosJugador, String[][] tablero, List<Carro> carros,
+	public Tablero(int numerosFila, int numerosColumnas, int carrosJugador, char[][] tablero, List<Carro> carros,
 			List<Huevo> huevos, Scanner lector) {
 		this.numerosFila = numerosFila;
 		this.numerosColumnas = numerosColumnas;
@@ -113,59 +113,69 @@ public class Tablero {
 
 
 	public static void CrearCarro() {
-		
+
 		JOptionPane.showMessageDialog(null, "Ingresa informacion Carros");
-		
-		
-		
-		
+
+
+
+
 		for (int i = 1; i < 4; i++) {
-				
+
 			String fechaIngreso = JOptionPane.showInputDialog("Ingrese Fecha Ingreso de Kromis: " + i);
 			String ocupantes = JOptionPane.showInputDialog("Ingrese Ocupantes de Kromis: " + i);
-		
-	
+
+
 		}
-			
+
 		for (int j = 1; j < 8; j++) {
-			
+
 			String distaciaTiro = JOptionPane.showInputDialog("Ingrese Distancia de Tiro de Caguano: " + j);
 			String colorConfeti = JOptionPane.showInputDialog("Ingrese color de confeti de Caguano: " + j);
-		
+
 		}
-		
+
 		for (int k = 1; k < 11; k++) {
-			
+
 			String nivelArmadura = JOptionPane.showInputDialog("Ingrese Nivel Armadura Trupalla: " + k) ;
 
 		}	
-		
+
 	}	
-			
-	
+
+
 	public static void LanzarHuevo() {
-		
+
 	}
-	
-	
+
+
 	public static void MostarMatriz() {
-		
+		for (int i = 0; i < 15; i++) {
+			System.out.print("[ ]");
+			for (int j = 0; j <15; j++) {
+
+				System.out.print(tablero[i][j]+"[ ]");
+
+
+			}
+			System.out.println("");
+		}
+
 	}
-	
-	
+
+
 	public static void CalcularPuntaje(){
-	
+
 	}
-	
+
 	public static void CrearKromi() {
 	}
-	
+
 	public static void CrearCaguanos() {
 	}
-	
+
 	public static void CrearTrupalla() {
 	}
-	
-	
+
+
 }
 
